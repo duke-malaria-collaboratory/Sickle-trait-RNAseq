9d0e69f9515cdc0002a45f739cb9d346ade79df2
bertfordley <rmosele4@vols.utk.edu>
2019-11-05 16:43:54 -0500

    added DLxJTK analysis on averaged datasets. Added protein_export_genes dir

--- DLxJTK_TAKT_runs_avg/run_takt.sh
+++ DLxJTK_TAKT_runs_avg/run_takt.sh
@@ -9,20 +9,20 @@
 # are RNA delayed between traits of each parasite: AS 3D7 vs AA 3D7, AS FUP vs AA FUP.
 
 ECHO AS 3D7 vs AA 3D7
-python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AS_3D7_means.txt -t2 AA_3D7_means.txt -c 10000 -s 15 -o 'Results'
+python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AS_3D7_1stage_periodic_jtk05.txt -t2 AA_3D7_1stage_periodic_jtk05.txt -c 10000 -s 15 -o 'Results'
 ECHO ------------------------------------
 
 ECHO AS FUP vs AA FUP
-python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AS_FUP_means.txt -t2 AA_FUP_means.txt -c 10000 -s 15 -o 'Results'
+python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AS_FUP_1stage_periodic_jtk05.txt -t2 AA_FUP_1stage_periodic_jtk05.txt -c 10000 -s 15 -o 'Results'
 ECHO ------------------------------------
 
 
 # are RNA delayed between parasites in AS: AS 3D7 vs AS FUP
 ECHO AS 3D7 vs AS FUP
-python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AS_3D7_means.txt -t2 AS_FUP_means.txt -c 10000 -s 15 -o 'Results'
+python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AS_3D7_1stage_periodic_jtk05.txt -t2 AS_FUP_1stage_periodic_jtk05.txt -c 10000 -s 15 -o 'Results'
 ECHO ------------------------------------
 
 # are RNA delayed between parasites in AA: AA 3D7 vs AA FUP
 ECHO AA 3D7 vs AA FUP
-python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AA_3D7_means.txt -t2 AA_FUP_means.txt -c 10000 -s 15 -o 'Results'
+python ../Sickle-trait-RNAseq/TAKT/python/rna_rna_diss.py -t1 AA_3D7_1stage_periodic_jtk05.txt -t2 AA_FUP_1stage_periodic_jtk05.txt -c 10000 -s 15 -o 'Results'
 ECHO ------------------------------------
